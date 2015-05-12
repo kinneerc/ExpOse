8c35ef51b444249fff717304dc561dd1dbf77c33
Cody Kinneer <kinneerc@allegheny.edu>
2015-05-12 13:47:54 -0400

    now generates a schema spec instead of trying to do a full schema

--- src/edu/allegheny/schemaexperiment/schemagen/Generator.java
+++ src/edu/allegheny/schemaexperiment/schemagen/Generator.java
@@ -6,90 +6,44 @@ import java.util.*;
 import java.lang.reflect.*;
 
 public class Generator{
-    public Schema generate(int tables, int columns, int notnulls, int primaryKeys, int foriegnKeys, int checks, int uniques){
 
-        // first get a basic schema of tables and columns
-        Schema ans = genTabCol(tables,columns);
-        // now we'll start to add constraints
-        // first, primary keys
-        addPKeys(ans,primaryKeys);
-
-        return ans;
-    }
-
-    /**
-     * Randomly add a set number of primary keys to the schema
-     */
-    private void addPKeys(Schema s,int pkeys){
-    List<Table> tbs = s.getTables();
-
-    if (tbs.size() < pkeys)
-        throw new SchemaGenException("Cannot generate schema with more PRIMARY KEY constraints than tables");
-
-    int npkeys = s.getPrimaryKeyConstraints().size();
-
-    if (npkeys + pkeys > tbs.size())
-        throw new SchemaGenException("Cannot generate schema with more PRIMARY KEY constraints than tables");
-    
-    Collections.shuffle(tbs);
-
-    int tbsi = 0;
-    for (int i = 0; i < pkeys; i++){
-        Table cur;
-        do{
-        cur = tbs.get(tbsi++);
-        }while(s.getPrimaryKeyConstraint(cur) != null);
-        
-        List<Column> cols = cur.getColumns();
-
-        Collections.shuffle(cols);
-
-        s.createPrimaryKeyConstraint(cur,cols.get(0));
-
-    }
-}
-
-
-    private Schema genTabCol(int tables, int columns){
-
-        Schema ans = new Schema("rs");
-
-        // determine how columns will be assigned to tables
-        // index = table, value = num of columns
-        int[] map = Util.group(tables,columns);
-
-        // add a table to the schema for each table
-        for (int t = 0; t < tables; t++){
-            Table cur = ans.createTable("rt");
-            // for each table, create the decided number of columns
-            // add a column to the schema for each col
-            // for now, a random datatype will suffice
-            for (int c = 0; c < map[t]; c++){
-                cur.createColumn("rc",randDataType());
-            }
-
-        }
-
-        return ans;
-
-    }
-
-    private DataType randDataType(){
-        Class f = (new DataTypeFactory()).getClass();
-        Method methods[] = f.getMethods();
+    public SchemaSpec randomSchema(int tables, int columns, int notnulls, int primaryKeys, int foriegnKeys, int checks, int uniques){
 
         Random rand = new Random();
 
-        try{
-            Object dt = methods[rand.nextInt(methods.length)].invoke(null);
+        // split up columns randomly
+        int[] colSplitPoints = Util.groupSplitPoints(tables,columns);
 
-            return (DataType) dt;
-        }catch(IllegalAccessException e){
-            e.printStackTrace();
-        }catch(InvocationTargetException e){
-            e.printStackTrace();
+        // randomly assign datatypes to columns
+        int[] colDataType = new int[columns];
+        for (int count = 0; count<columns; count++){
+            colDataType[count] = rand.nextInt(DataTypeFactory.numSupportedTypes);
         }
-        return null;
+
+        // NOTICE::funny interpretation, primarykeys = the number of columns in a primary key
+        // so then pick which columns are in a primary key
+        boolean[] pkeys = new boolean[columns];
+        for (int count = 0; count < primaryKeys; count++){
+            int randCol;
+            do{
+                randCol = rand.nextInt(columns);
+            }while(pkeys[randCol]==true);
+                pkeys[randCol] = true;
+        }
+
+        // now pick which columns are not null
+        boolean[] notNulls = new boolean[columns];
+        for (int count = 0; count < notnulls; count++){
+            int randCol;
+            do{
+                randCol = rand.nextInt(columns);
+            }while(notNulls[randCol]==true);
+                notNulls[randCol] = true;
+        }
+        // TODO fkeys, uniques, and checks
+
+        return SchemaSpec(tables,colSplitPoints,colDataType,)
+        
     }
 
 }
