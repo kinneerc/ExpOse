c21ee9b448c73d657ab358077b361319b5cdc5fd
Cody Kinneer <kinneerc@allegheny.edu>
2015-06-01 16:44:16 -0400

    added test script for new generator based doublers

---
+++ TestRunNew.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+declare -a criterions=(
+"ICC"
+)
+
+declare -a datagenerators=(
+"directedRandom"
+)
+
+declare -a schemas=(
+"UnixUsage"
+"BioSQL"
+)
+
+declare -a doublersWSub=(
+"columns"
+"primaryKeys"
+"foriegnKeys"
+"uniques"
+"all"
+)
+
+declare -a doublersNSub=(
+"tables"
+"notNulls"
+)
+
+
+declare -a subDoublers=(
+"number"
+"size"
+"both"
+)
+
+export CLASSPATH="bin:lib/*:."
+
+classname="edu.allegheny.schemaexperiment.schemagen.SchemaGenExperiment"
+
+for criterion in "${criterions[@]}" 
+do
+    for datagenerator in "${datagenerators[@]}"
+    do
+        for schema in "${schemas[@]}"
+        do
+            for doubler in "${doublersWSub[@]}"
+            do
+                for subDoubler in "${subDoublers[@]}"
+                do
+                    java $classname --criterion $criterion --datagenerator $datagenerator --doubler $doubler --subFeature $subDoubler --maxTime 50 --minDoubles 15 --overwrite
+                done
+            done
+            for doubler in "${doublersNSub[@]}"
+            do
+                java $classname --criterion $criterion --datagenerator $datagenerator --doubler $doubler --maxTime 50 --subFeature number --minDoubles 15 --overwrite
+            done
+        done
+    done
+done
